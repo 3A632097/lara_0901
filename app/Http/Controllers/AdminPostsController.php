731508d5d0d2da2ab5a7da8c47fe899bf7208be0
Ivy Lin <a0981792089@gmail.com>
2019-11-26 23:19:07 +0800

    練習3-1使用 Model 查詢資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
 
 class AdminPostsController extends Controller
 {
     public function index()
     {
-        return view('admin.posts.index');
+        //return view('admin.posts.index');
+        
+        //使用 Model 查詢資料
+        $post=Post::orderBy('created_at', 'DESC')->get();
+        $data=['posts'=>$post];
+        return view('admin.posts.index', $data);
     }
 
     public function create()
