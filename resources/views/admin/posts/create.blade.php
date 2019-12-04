9bc87638b769ff0831890e318b9cb20add698ea3
Ivy Lin <a0981792089@gmail.com>
2019-12-04 15:08:32 +0800

    修改練習5-5有錯內容

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,60 +3,52 @@
 @section('title', '新增文章')
 
 @section('content')
-<!-- Page Heading -->
-<div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">
-            新增文章 <small>請輸入文章內容</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li class="active">
-                <i class="fa fa-edit"></i> 文章管理
-            </li>
-        </ol>
-    </div>
-</div>
-<!-- /.row -->
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                新增文章 <small>請輸入文章內容</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active">
+                    <i class="fa fa-edit"></i> 文章管理
+                </li>
+            </ol>
         </div>
     </div>
-</div>
-<!-- /.row -->
+    <!-- /.row -->
 
-<div class="row">
-    <div class="col-lg-12">
-        <form role="form">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
 
+    <div class="row">
+        <div class="col-lg-12">
             <form action="/admin/posts" method="POST" role="form">
-
-                <!-- 可產生一組安全檢驗碼 -->
                 {{ csrf_field() }}
 
                 <div class="form-group">
                     <label>標題：</label>
-                    <!--<input class="form-control" placeholder="請輸入文章標題">-->
                     <input name="title" class="form-control" placeholder="請輸入文章標題">
                 </div>
 
                 <div class="form-group">
                     <label>內容：</label>
-                    <!--<textarea class="form-control" rows="10"></textarea>-->
                     <textarea name="content" class="form-control" rows="10"></textarea>
                 </div>
 
                 <div class="form-group">
                     <label>精選？</label>
-                    <!--<select class="form-control">-->
                     <select name="is_feature" class="form-control">
                         <option value="0">否</option>
                         <option value="1">是</option>
                     </select>
-
                 </div>
 
                 <div class="text-right">
@@ -68,8 +60,7 @@
                 <p>&nbsp;</p>
 
             </form>
-
+        </div>
     </div>
-</div>
-<!-- /.row -->
+    <!-- /.row -->
 @endsection
